ee32be3c413d7c2e4bfbb2eb95aab8ae753ea43f
kishan817835 <kishan.ln0007@gmail.com>
2025-12-17 09:52:01 +0530

    solve error

--- portfolio/src/components/AptitudeTest/CareerAptitudeTest.jsx
+++ portfolio/src/components/AptitudeTest/CareerAptitudeTest.jsx
@@ -496,6 +496,18 @@ const handleConsultationSubmit = async (consultationData) => {
           </button>
         </div>
       </div>
+      
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       </>
     );
   }
