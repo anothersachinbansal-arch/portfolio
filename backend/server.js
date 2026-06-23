b0b0f95f4585a3e20fb43c8a08a9105dcb49f0d0
kishan8178 <kishan817835@gmail.com>
2026-06-23 19:46:57 +0530

    fixed achievers

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3000",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
